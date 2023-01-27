6620a93db7902619f0be7a4cbd8be2d5ace977ee
Raissa <barata.raissa@gmail.com>
2023-01-27 15:53:53 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./componentes/Banner/Banner";
+import Formulario from "./componentes/Formulario";
+import { useState } from "react";
+import Time from "./componentes/Time";
+import Rodape from "./componentes/Rodape";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#d9f7e9;",
+      corSecundaria: "#57c278;",
+    },
+
+    {
+      nome: "Front-End",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
+    },
+
+    {
+      nome: "Data Science",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#F0F8E2",
+    },
+
+    {
+      nome: "DevOps",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
+    },
+
+    {
+      nome: "UX/UI Design",
+      corPrimaria: "#D86EBF",
+      corSecundaria: "#FAE5F5",
+    },
+
+    {
+      nome: "Mobile",
+      corPrimaria: "#FEBA05",
+      corSecundaria: "#FFF5D9",
+    },
+
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8A29",
+      corSecundaria: "#FFEEDF",
+    },
+  ];
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Formulario
+        times={times.map((time) => time.nome)}
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
+
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.time == time.nome
+          )}
+        />
+      ))}
+      <Rodape />
     </div>
   );
 }
